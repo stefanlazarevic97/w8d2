5758e47773f52f2161f35e33d7f8d2766245d729
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-11 16:52:31 -0700

    add phase 3

--- intro-javascript-exercises/phase-3-iteration.js
+++ intro-javascript-exercises/phase-3-iteration.js
@@ -1,14 +1,34 @@
 Array.prototype.bubbleSort = function() {
-    array = this.slice(1);
+    array = this.slice();
     let sorted = false;
 
     while(!sorted) {
         sorted = true;
 
         array.forEach( (ele, i) => {
-            if (ele < arr[i + 1]) {
-                new_ele
+            if (ele > array[i + 1]) {
+                [array[i], array[i + 1]] = [array[i + 1], array[i]];
+
+                sorted = false;
             }
+        })
+    }
+
+    return array;
+};
+
+// console.log([3, 1, -5, 0, -2, 9, 4].bubbleSort()) // => [-5, -2, 0, 1, 3, 4, 9]
+
+String.prototype.substrings = function() {
+    let substrings = [];
+
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j <= this.length; j++) {
+            substrings.push(this.slice(i, j));
         }
     }
+
+    return substrings;
 };
+
+// console.log("Hello".substrings()) // => ["H", "He", "Hel", "Hell", "Hello", "e", "el", "ell", "ello", "l", "ll", "llo", "l", "lo", "o"]
